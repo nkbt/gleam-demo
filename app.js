9406f33cd18f9ba9f2975a9b41963dd78774c5f8
Nik Butenko <nik@butenko.me>
2013-08-27 01:12:58 +1000

    Wired up error handler and response renderer

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-/**
- * Module dependencies.
- */
+'use strict';
 
 var express = require('express');
 var http = require('http');
 var path = require('path');
 var bootstrap = require('static-bootstrap');
 
-
-var dispatcher = require('dispatcher');
-dispatcher.setRoot(path.join(__dirname, 'controllers'));
-
+var responseRenderer = require('./response-renderer');
+var errorHandler = require('./error-handler');
 
 var gleam = require('gleam');
 gleam.setRoot(path.join(__dirname, 'gleams'));
+
+var dispatcher = require('dispatcher');
+dispatcher.setRoot(path.join(__dirname, 'controllers'));
+dispatcher.setResponseRenderer(responseRenderer(gleam));
 
 var app = express();
 
@@ -24,21 +24,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/js/entity', gleam.serveEntity);
 app.use(express.favicon());
 
-app.use(bootstrap(path.join(__dirname, 'public'), 'index.html'));
+app.use(bootstrap(path.join(__dirname, 'public')));
 
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.cookieParser('qwerty1234'));
 app.use(express.session());
 app.use(app.router);
+app.use(errorHandler(dispatcher));
 
-// development only
-if ('development' == app.get('env')) {
-	app.use(express.errorHandler());
-}
 
 app.get('/', dispatcher.route('index', 'index'));
 app.get('/users', dispatcher.route('user', 'index'));
+app.get('*', dispatcher.route('error', 'index'));
 
 http.createServer(app).listen(app.get('port'), function () {
 	console.log('Express server listening on port ' + app.get('port'));
