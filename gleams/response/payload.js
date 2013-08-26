4d4e0f4c009db253d0f9546323ae1a7980f2c258
Nik Butenko <nik@butenko.me>
2013-08-27 02:05:03 +1000

    Fix Payload gleam: deprecated property "type" should not be used

--- gleams/response/payload.js
+++ gleams/response/payload.js
@@ -22,16 +22,6 @@ var PayloadEntity = {
 	 */
 	validateData: function (value) {
 		return _.isObject(value) || _.isArray(value);
-	},
-
-	setData: function (value) {
-		if (_.isArray(value) && !_.isEmpty(value) && !_.isUndefined(value[0].namespace)) {
-			this.set({type: value[0].namespace});
-		} else if (!_.isUndefined(value.namespace)) {
-			this.set({type: value.namespace});
-		}
-
-		this.data = value;
 	}
 };
 
