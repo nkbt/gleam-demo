9a9073b46cd74c6943b7fe715fa4f263f5a57ca5
Nik Butenko <nik@butenko.me>
2013-08-27 02:03:22 +1000

    Gleams: Test

---
+++ gleams/test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @mixes {AbstractEntity}
+ */
+var TestEntity = {
+
+	/**
+	 * @type {String}
+	 */
+	name: null,
+	/**
+	 * @type {Number}
+	 */
+	value: null,
+	/**
+	 * @type {Date}
+	 */
+	createdOn: null
+
+};
+
+
+exports.Entity = TestEntity;
