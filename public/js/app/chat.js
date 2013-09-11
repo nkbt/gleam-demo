4e5a4234f6e0bb3a96d7598e041a341f1c1b527e
Nik Butenko <nik@butenko.me>
2013-09-11 23:52:04 +1000

    Frontend: Controllers and Views added according to the new Dispatcher

--- public/js/app/chat.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/*jshint browser:true */
-/*jslint browser:true */
-
-define('app/test', ['dom', 'underscore', 'lib/request', 'lib/messenger'], function ($, _, request) {
-	var $body = $(document.body);
-
-
-});
