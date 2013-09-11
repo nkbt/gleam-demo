990a1286823fc79acdd881ba0595389c9d60d036
Nik Butenko <nik@butenko.me>
2013-09-12 01:32:28 +1000

    Frontend: Cascaded rendring for templates and blocks in Layout

--- public/js/lib/layout.js
+++ public/js/lib/layout.js
@@ -29,16 +29,19 @@ define('lib/layout', ['dom', 'underscore', 'lib/app'], function ($, _, app) {
 
 
 	function onRenderBlock(event, blockName, content) {
-		var $element = $(event.target).closest('.lib_layout');
-		$element.find('.lib_layout[data-lib_layout-block="' + blockName + '"]')
-			.clean()
-			.append(content)
-			.trigger('lib/layout:renderBlock:done', [blockName]);
+		var $element = $(event.target).closest('.lib_layout'),
+			$content = $($.parseHTML(content.trim()));
+
+		$element.attr('data-lib_layout-rendered', null);
+		$element.find('.lib_layout[data-lib_layout-block="' + blockName + '"]').html($content);
+		$content.trigger('lib/layout:renderBlock:done', [blockName]);
 	}
 
 	app.$root
 		.on('lib/layout:render', null, onRender)
-		.on('lib/layout:renderBlock', null, onRenderBlock);
+		.on('lib/layout:renderBlock', null, onRenderBlock)
+		.on('lib/layout:renderBlock:done', '.lib_layout:not([data-lib_layout-rendered])', onRender);
+	
 
 
 	return {
