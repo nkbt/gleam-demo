deabc3e153d55f179602414bee7be82ae1b1543a
Nik Butenko <nik@butenko.me>
2013-09-13 23:57:58 +1000

    Request updated with HTTP modes

--- public/js/lib/request.js
+++ public/js/lib/request.js
@@ -12,21 +12,21 @@ define(
 
 
 		function successHandler(responseEntity, callback) {
-			var error = _(responseEntity.get('message')).reduce(function (result, messageEntity) {
+			var errorMessage = _(responseEntity.get('message')).reduce(function (result, messageEntity) {
 				var type = messageEntity.isError && messenger.TYPE_ERROR || messenger.TYPE_MESSAGE;
 				app.$root.trigger('lib/messenger:show', [type, messageEntity.text]);
 				return result === null && messageEntity.isError && messageEntity.text || null;
-			}, null);
-
+			}, null),
+				error = errorMessage && new Error(errorMessage) || null;
 
 			// For now we use only one payloadEntity per response
-			return callback(error && new Error(error) || null, responseEntity.get('payload').shift());
+			return callback(error, responseEntity.get('payload').shift());
 		}
 
 
 		function parseResponse(text, callback) {
 			return fromJson(text, function (error, responseEntity) {
-				if (error === null) {
+				if (!error) {
 					successHandler(responseEntity, callback);
 				} else {
 					veryBadErrorHandler(error, callback);
@@ -48,25 +48,29 @@ define(
 			return callback(error);
 		}
 
+		function run(method, url, data, callback) {
+			$.ajax({
+				url: url,
+				type: method,
+				data: JSON.parse(JSON.stringify(data)),
+				dataType: 'text',
+				success: function (json) {
+					return parseResponse(json, callback);
+				},
+				error: function (responseText) {
+					return errorHandler(responseText, callback);
+				}
+			});
+		}
+		run.METHOD_GET = 'GET';
+		run.METHOD_POST = 'POST';
+		run.METHOD_PUT = 'PUT';
+		run.METHOD_PATCH = 'PATCH';
+		run.METHOD_DELETE = 'DELETE';
+		run.METHOD_HEAD = 'HEAD';
+		run.METHOD_OPTIONS = 'OPTIONS';
 
-		return {
-			get: function (url, data, callback) {
-				data = data || {};
-				data.json = 1;
-				$.ajax({
-					url: url,
-					type: 'GET',
-					data: data,
-					dataType: 'text',
-					success: function (json) {
-						return parseResponse(json, callback);
-					},
-					error: function (responseText) {
-						return errorHandler(responseText, callback);
-					}
-				});
-			}
-		};
+		return run;
 
 	}
 );
