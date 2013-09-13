a18c6b4e7e2360f1ad76267e1c3207278bbf81b6
Nik Butenko <nik@butenko.me>
2013-09-13 20:38:56 +1000

    Frontend: Request module - updated to comply with async convention

--- public/js/lib/request.js
+++ public/js/lib/request.js
@@ -3,70 +3,70 @@
 /*jslint browser:true */
 
 
-define('lib/request', ['dom', 'underscore', 'entity/from-json', 'entity/response/message'], function ($, _, fromJson, MessageEntity) {
+define(
+	'lib/request',
+	[
+		'dom', 'underscore', 'lib/app', 'lib/messenger', 'entity/from-json'
+	],
+	function ($, _, app, messenger, fromJson) {
 
-	var $body = $(document.body),
-		successHandler,
-		errorHandler,
-		veryBadErrorHandler;
 
-	successHandler = function (responseEntity, callback) {
-		_(responseEntity.get('message')).each(function (messageEntity) {
-			$body.trigger('messenger:show', [messageEntity]);
-		});
-		// For now we use only one payloadEntity per response
-		return callback(responseEntity.get('payload')[0]);
-	};
+		function successHandler(responseEntity, callback) {
+			var error = _(responseEntity.get('message')).reduce(function (result, messageEntity) {
+				var type = messageEntity.isError && messenger.TYPE_ERROR || messenger.TYPE_MESSAGE;
+				app.$root.trigger('lib/messenger:show', [type, messageEntity.text]);
+				return result === null && messageEntity.isError && messageEntity.text || null;
+			}, null);
 
-	errorHandler = function (response, callback) {
-		if (!response.responseText) {
-			return veryBadErrorHandler();
+
+			// For now we use only one payloadEntity per response
+			return callback(error && new Error(error) || null, responseEntity.get('payload').shift());
 		}
-		return fromJson(response.responseText, function (error, responseEntity) {
-			if (error === null) {
-				successHandler(responseEntity, callback);
-			} else {
-				veryBadErrorHandler(error);
-			}
-		});
-	};
 
-	veryBadErrorHandler = function (error) {
-		var text;
-		if (!_.isEmpty(error)) {
-			// TODO: log this error to log-server
-			text = error instanceof Error ? error.message : error;
-			console.log(text);
-		}
-		$body.trigger('messenger:show', [new MessageEntity({
-			isMessage: true,
-			text: "OMFG!!!!!11 Very bad error happened, report was sent, developers will be punished."
-		})]);
-	};
 
-	return {
-		get: function (url, data, callback) {
-			data = data || {};
-			data.json = 1;
-			$.ajax({
-				url: url,
-				type: 'GET',
-				data: data,
-				dataType: 'text',
-				success: function (json) {
-					return fromJson(json, function (error, responseEntity) {
-						if (error === null) {
-							successHandler(responseEntity, callback);
-						} else {
-							veryBadErrorHandler(error);
-						}
-					});
-				},
-				error: function (responseText) {
-					return errorHandler(responseText, callback);
+		function parseResponse(text, callback) {
+			return fromJson(text, function (error, responseEntity) {
+				if (error === null) {
+					successHandler(responseEntity, callback);
+				} else {
+					veryBadErrorHandler(error, callback);
 				}
 			});
 		}
-	};
 
-});
+
+		function errorHandler(response, callback) {
+			if (!response.responseText) {
+				return veryBadErrorHandler(new Error("OMFG!!!!!11 Very bad error happened, report was sent, developers will be punished."), callback);
+			}
+			return parseResponse(response.responseText, callback);
+		}
+
+
+		function veryBadErrorHandler(error, callback) {
+			app.$root.trigger('lib/messenger:show', [messenger.TYPE_ERROR, error.message]);
+			return callback(error);
+		}
+
+
+		return {
+			get: function (url, data, callback) {
+				data = data || {};
+				data.json = 1;
+				$.ajax({
+					url: url,
+					type: 'GET',
+					data: data,
+					dataType: 'text',
+					success: function (json) {
+						return parseResponse(json, callback);
+					},
+					error: function (responseText) {
+						return errorHandler(responseText, callback);
+					}
+				});
+			}
+		};
+
+	}
+);
