501748f9e2943837cd5f37d8bced337ac9ecd591
Nik Butenko <nik@butenko.me>
2013-09-13 23:58:21 +1000

    DOM form element helper module

---
+++ public/js/lib/dom/form.js
@@ -0,0 +1,47 @@
+"use strict";
+
+
+define('lib/dom/form', ['module', 'dom', 'underscore'], function (module, $, _) {
+	var config = _.defaults(module.config(), {
+		disabledClass: 'disabled',
+		elementsSelector: 'input, textarea, select'
+	});
+
+	function values(form) {
+		var $form = $(form),
+			data = $form.serializeArray();
+		console.log('data', data);
+		return _.object(_.pluck(data, 'name'), _.pluck(data, 'value'));
+	}
+
+	function clear(form) {
+		var $form = $(form);
+		$form.find(config.elementsSelector).val(null);
+		return $form;
+	}
+
+	function disable(form) {
+		var $form = $(form);
+		$form.addClass(config.disabledClass)
+			.attr('data-lib_dom_form-disabled', true)
+			.find(config.elementsSelector).prop('disabled', true);
+		return $form;
+	}
+
+	function enable(form) {
+		var $form = $(form);
+		$form.removeClass(config.disabledClass)
+			.attr('data-lib_dom_form-disabled', null)
+			.find(config.elementsSelector).prop('disabled', false);
+		return $form;
+	}
+
+
+	return {
+		values: values,
+		clear: clear,
+		disable: disable,
+		enable: enable
+	};
+
+});
